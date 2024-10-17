ec6e9958b2ed31c1e3f8fac0397eee4747cdbc6c
GSmithApps <14.gsmith.14@gmail.com>
2024-10-16 22:51:39 -0500

    good flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,24 @@
 {
   description = "Development environment with vsce and Node.js 20";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  outputs = { self, nixpkgs }: 
-    let
-      system = "x86_64-linux";  # or "x86_64-darwin" for macOS, etc.
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          vsce
-          nodejs_20
-        ];
-        shellHook = ''
-          echo "Welcome to the CSV-aligner development environment!"
-          echo "Node.js version: $(node --version)"
-          echo "vsce version: $(vsce --version)"
-        '';
-      };
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            vsce
+            nodejs_20
+          ];
+          shellHook = ''
+            echo "Welcome to the CSV-aligner development environment!"
+            echo "Node.js version: $(node --version)"
+            echo "vsce version: $(vsce --version)"
+          '';
+        };
+      }
+    );
 }
