b114c01bd7d1b44e1f626437b6ab45ce7570589e
Grant Smith <14.gsmith.14@gmail.com>
2025-09-14 08:33:32 -0500

    got jsdoc up and running

--- src/helpers/getMaxColumnWidthsFromCellLengths.ts
+++ src/helpers/getMaxColumnWidthsFromCellLengths.ts
@@ -1,4 +1,27 @@
 
+/**
+ * @file This file contains helper functions for parsing CSV data.
+ * @author Grant Smith
+ * @version 1.0.0
+ */
+
+/**
+ * This function is pretty self-explanatory. When a matrix
+ * of cell lengths is passed in, it will look through each of
+ * those and for each column, it will return the maximum length
+ * inside that column. 
+ * 
+ * One potential area of improvement would be to allow a second
+ * argument, which is a historic max length, and then that could
+ * be taken into account and the max of that and the new one
+ * would be taken. Sort of like a cache.
+ */
+
+/**
+ * Get the list of column widths from the matrix of cell lengths
+ * @param cellLengths the matrix of cell lengths
+ * @returns an array of the column widths
+ */
 export function getMaxColumnWidthsFromCellLengths(
   cellLengths: number[][]
 ): number[] {
